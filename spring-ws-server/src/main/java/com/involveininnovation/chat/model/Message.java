6117f540a78b208feee9ddda44f5111c2419e8bf
Sahil Agarwal <sahil11875@gmail.com>
2022-07-17 16:20:56 +0530

    Web APP

---
+++ spring-ws-server/src/main/java/com/involveininnovation/chat/model/Message.java
@@ -0,0 +1,24 @@
+package com.involveininnovation.chat.model;
+
+import lombok.*;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Message {
+    private String senderName;
+    private String receiverName;
+    private String message;
+    private String date;
+    private Status status;
+	public String getReceiverName() {
+		return receiverName;
+	}
+	public void setReceiverName(String receiverName) {
+		this.receiverName = receiverName;
+	}
+}
